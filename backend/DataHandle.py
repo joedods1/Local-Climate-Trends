e42d2eb2bf334fbb48f9c380a03bc4c2ccc1df4b
Jd <joedods1@gmail.com>
2017-01-23 19:45:29 +0000

    2 cumlat distri currently working

--- backend/DataHandle.py
+++ backend/DataHandle.py
@@ -5,15 +5,16 @@ from backend import varfuncs as vf
 from jdcal import jd2gcal
 import numpy as np
 from datetime import datetime
-
+from scipy import stats
 class DataRequest:
-    def __init__(self, datastr, lat_rng, longi_rng, datestr, seasonstr):   #define the various parameters,
+    def __init__(self, datastr, lat_rng, longi_rng, startdatestr, enddatestr, seasonstr):   #define the various parameters,
                                                                 #wether want rain fall data, 'rr',
         self.seasonstr=seasonstr                                                        #temp, 'tn', 'tq'
         self.datastr = datastr
         self.lat_rng = lat_rng
         self.longi_rng = longi_rng
-        self.datestr = datestr
+        self.startdatestr = startdatestr
+        self.enddatestr = enddatestr
         self.dataloc='/home/jd/climatedata/'                    # place holder
     def getdatearrays(self):
         (self.jdstart, self.jdstarttup)=vf.conv2jd(self.time.units) #convert the dates into jullian day       
@@ -29,7 +30,7 @@ class DataRequest:
         alllimits=[[152, 243], [335, 59], [60, 151], [244, 334]] # define the various DOY  limits [start, stop]
         if self.seasonstr==seasons[0]:
             limits=alllimits[0]
-        elif self.seasonstr==seasons[1]:
+        elif self.seasonstr==seasons[1]:                            ## bunch of conditionals to determine the season
             limits=alllimits[1]
         elif self.seasonstr==seasons[2]:
             limits=alllimits[2]
@@ -42,10 +43,18 @@ class DataRequest:
             mask = (self.doy >= limits[0])*(self.doy <=  limits[1])
         else:
             mask= (self.doy <= limits[0]) + (self.doy >=  limits[1])      ##apply the appropraite conditions
-        self.doy=np.ma.array(self.doy, mask=mask)
+        self.seasonmask=mask
+    def timereduction(self):                                                       ## this fucntion obtians the mask needed to only read in teh appropriate data
+        self.getdatearrays()
+        self.seasonreduction()
+        (self.jddatestart, self.jddatestarttup)=vf.conv2jd(self.startdatestr)
+        (self.jddateend, self.jddateendtup)=vf.conv2jd(self.enddatestr)
+        subsetmask=(self.jd_array>=self.jddatestart)*(self.jd_array<=self.jddateend)    ##mask teh region of interest ie. the 9 years specify when first calling class
+        self.timemask=self.seasonmask*subsetmask
+        self.timemasklist=np.where(self.timemask==True)[0].tolist()
         
     def readdata(self):
-        fullpath=self.dataloc+self.datastr+'_0.50deg_reg_1950-1964_v14.0.nc'
+        fullpath=self.dataloc+self.datastr+'_0.50deg_reg_v14.0.nc'
         # add relavent varibles to structure
         self.lat=ds(fullpath,'r').variables['latitude'][:]
         self.longi=ds(fullpath,'r').variables['longitude'][:]
@@ -54,10 +63,8 @@ class DataRequest:
         (self.longi_index, longi_reduced) = vf.condi_ind(self.longi, self.longi_rng)
         
         self.time=ds(fullpath,'r').variables['time']
-        self.full=ds(fullpath,'r')
-        
-        self.getdatearrays()
-        self.seasonreduction()
+        self.full=ds(fullpath,'r')        
+        self.timereduction()
         
 ##        (self.jdstart, self.jdstarttup)=vf.conv2jd(self.time.units) #convert the dates into jullian day
 ##        
@@ -65,8 +72,8 @@ class DataRequest:
 ##        self.doy=np.zeros(self.jd_array.size)                        # convert jd into DOY
 ##
 ##            
-        self.vari=ds(fullpath,'r').variables[self.datastr][:,self.lat_index,self.longi_index] #reduce the main data matrix
-        self.vari=self.vari.data[~self.vari.mask] #remove the mask just data array
+        self.vari=ds(fullpath,'r').variables[self.datastr][self.timemasklist,self.lat_index,self.longi_index] #reduce the main data matrix
+        self.vari_data=self.vari.data[~self.vari.mask] #remove the mask just data array
     
         
         
@@ -75,6 +82,36 @@ class ProcessData(DataRequest):
     def __init__(self, DataRequest_1, DataRequest_2):
         self.DataRequest_1=DataRequest_1
         self.DataRequest_2=DataRequest_2
+        
+    def cumdist(self,nedges):
+        self.minmaxvals=[min(self.DataRequest_1.vari_data.min(), self.DataRequest_2.vari_data.min()), max(self.DataRequest_1.vari_data.max(), self.DataRequest_2.vari_data.max())]        
+        vari_size=self.DataRequest_1.vari.shape
+
+        ## select for regions in which both data sets  have a sufficient number of data entries (here >400) 
+        self.looptup=np.where(((~self.DataRequest_1.vari.mask).sum(axis=0)>400)*((~self.DataRequest_1.vari.mask).sum(axis=0)>400)==True)
+
+        
+        self.histoarr1=np.zeros((nedges, len(self.looptup[0])))
+        self.histoarr2=np.zeros((nedges, len(self.looptup[0])))
+##        self.cumsum1=np.zeros((nedges, len(self.looptup[0])))
+##        self.cumsum2=np.zeros((nedges, len(self.looptup[0])))
+        for indx in range(len(self.looptup[0])):              
+            hist1, edges = np.histogram(self.DataRequest_1.vari.data[:,self.looptup[0][indx],self.looptup[1][indx]], range=(float(self.minmaxvals[0]),  float(self.minmaxvals[1])), bins=nedges, normed=True)                
+            self.histoarr1[:,indx]=hist1
+            dx=edges[1]-edges[0]
+##              hist1, edges = stats.(self.DataRequest_1.vari.data[:,self.looptup[0][indx],self.looptup[1][indx]])
+                
+                            
+            hist2, edges = np.histogram(self.DataRequest_2.vari.data[:,self.looptup[0][indx],self.looptup[1][indx]], range=(float(self.minmaxvals[0]),  float(self.minmaxvals[1])), bins=nedges, normed=True)                
+            self.histoarr2[:,indx]=hist2
+            
+                
+            
+            
+        self.cumsum1=np.cumsum(self.histoarr1,axis=0)*dx
+        self.cumsum2=np.cumsum(self.histoarr2,axis=0)*dx
+        self.edges=edges
+
 ##    def sanitycheck(self):
 ##        ##this fucntion checks the ranges for the data, makes sure the data is of teh same type ie rainfull, temp etc aswell as other things might include later on
 ##        if self.DataRequest_1.datastr == self.DataRequest_1.datastr:
@@ -92,32 +129,4 @@ class ProcessData(DataRequest):
 ##                
 ##        else
 ##            print('Trying to compare the distributions of different data')
-##            return False
-    
-        
-    def cumdist(self,nedges):
-        self.minmaxvals=[min(self.DataRequest_1.vari.min(), self.DataRequest_2.vari.min()), max(self.DataRequest_1.vari.max(), self.DataRequest_2.vari.max())]        
-        hist1, edges = np.histogram(self.DataRequest_1.vari, range=(self.minmaxvals[0],  self.minmaxvals[1]), bins=nedges, density=True)
-        self.cumsum1=np.cumsum(hist1)
-        
-        hist2, edges = np.histogram(self.DataRequest_2.vari, range=(self.minmaxvals[0],  self.minmaxvals[1]), bins=nedges, density=True)
-        self.cumsum2=np.cumsum(hist2)
-        
-        self.edges=edges
-        
-##        if sanitycheck(self.DataRequest_1.datastr, minmaxvals):
-##           self.edgearr=
-            
-
-            
-##        devisions
-        
-        #things that need to be considered, what are the limits of the data, how many bins do we want,
-        #do we want the cdf to be fitted with analytical function thereby reducing the amount of data stored 
-        #
-        
-
-
-
-
-        
+##            return False      
--- testscript.py
+++ testscript.py
@@ -3,16 +3,30 @@ import numpy as np
 from netCDF4 import Dataset as ds
 from backend.varfuncs import condi_ind
 from datetime import datetime
-
-datastr='rr'
+import matplotlib.pyplot as plt
+from scipy import stats
+datastr='tn'
 lat_rng=[35, 70]
 longi_rng=[-15, 15]
-seasonstr='summer'
-datestr1='1950-01-01'
-datestr2='2000-01-01'
-tempy1=dh.DataRequest(datastr, lat_rng, longi_rng, datestr1, seasonstr)
+seasonstr='winter'
+startdatestr1='1962-01-01'
+enddatestr1='1970-12-31'
+startdatestr2='2007-01-01'
+enddatestr2='2015-12-31'
+tempy1=dh.DataRequest(datastr, lat_rng, longi_rng, startdatestr1,enddatestr1, seasonstr)
 tempy1.readdata()
-tempy2=dh.DataRequest(datastr, lat_rng, longi_rng, datestr2, seasonstr)
+tempy2=dh.DataRequest(datastr, lat_rng, longi_rng,  startdatestr2,enddatestr2, seasonstr)
 tempy2.readdata()
-a=dh.ProcessData(tempy1,tempy2)
 
+
+
+
+
+processed=dh.ProcessData(tempy1,tempy2)
+processed.cumdist(100)
+x=processed.edges
+dx=processed.edges[1]-processed.edges[0]
+a=np.cumsum(processed.histoarr1,axis=0)*dx
+a2=np.cumsum(processed.histoarr2,axis=0)*dx
+plt.plot(x[range(1,101)], a[:,150], x[range(1,101)], a2[:,150])
+plt.show()
